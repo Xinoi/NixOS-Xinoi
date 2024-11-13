b99e2bbefe27f3ba45b0cc7cb3570b6cb6f4484a
Johannes Zaunrith <zaunrithj@gmail.com>
2024-11-13 20:54:07 +0100

    added fenix flake

--- flake.nix
+++ flake.nix
@@ -2,22 +2,32 @@
 description = "A simple NixOS flake";
 
 inputs = {
-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-nvim-flake.url = "github:Xinoi/NeoVim-Flake/main";
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  nvim-flake.url = "github:Xinoi/NeoVim-Flake/main";
+  fenix.url = "github:nix-community/fenix";
 };
 
-outputs = {self, nixpkgs, nvim-flake, ...}@inputs: {
-    nixosConfigurations.nixos-xinoi = nixpkgs.lib.nixosSystem {
+outputs = {self, ...}@inputs: let
+  pkgs = self.inputs.nixpkgs {system = "x86_64-linux";};
+  neovim = self.inputs.nvim-flake;
+  rust = self.inputs.fenix.packages.default.default;
+in
+{
+    nixosConfigurations.nixos-xinoi = pkgs.lib.nixosSystem {
 	    system = "x86_64-linux";
 	    specialArgs = { inherit inputs; };
-	    modules = [
+	    modules = [ 
+          {
+            nixpkgs.overlays = [neovim.overlays.default];
+            environment.systemPackages = [
+              rust
+            ];
+          }
+
 		    ./configuration.nix
         ./fonts.nix
 	    ];
     };
-
-    nixpkgs.overlays = [ nvim-flake.overlays.default ];
-
 };
 }
 
