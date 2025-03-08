2fe5a305b6f3442d207891ac24c866a74e55c97a
Xinoi <zaunrithj@gmail.com>
2025-03-08 21:15:13 +0100

    add drivers module

--- configuration.nix
+++ configuration.nix
@@ -14,21 +14,12 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.kernelModules = [ "amdgpu" ];
 
-  hardware.enableRedistributableFirmware = true; 
-  hardware.firmware = [ pkgs.linux-firmware ];
-
   networking.hostName = "nixos-xinoi"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-    
-  hardware.graphics = {
-    enable = true; 
-  };
-
-  chaotic.mesa-git.enable = true;
 
   # Enable networking
   networking = {
@@ -150,30 +141,6 @@
   # Configure console keymap
   console.keyMap = "de";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-  # drawing tablet support
-  hardware.opentabletdriver.enable = true;
-
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.xinoi = {
     isNormalUser = true;
@@ -207,8 +174,6 @@
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
   };
-
-  programs.gamemode.enable=true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
