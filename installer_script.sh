3b1c70c8491729b629b12f5c1b36c87115479bdb
Xinoi <zaunrithj@gmail.com>
2025-03-11 18:47:50 +0100

    better partitionng

--- installer_script.sh
+++ installer_script.sh
@@ -27,10 +27,14 @@ function partition {
   parted /dev/"$DEVICE" -- mkpart ESP fat32 1MB 512MB > /dev/null 2>&1
   parted /dev/"$DEVICE" -- set 3 esp on > /dev/null 2>&1
   
-  ROOT_PART=$(lsblk -ln -o NAME /dev/"$DEVICE" | grep -E "${DEVICE}[^[:space:]]+1$")
-  SWAP_PART=$(lsblk -ln -o NAME /dev/"$DEVICE" | grep -E "${DEVICE}[^[:space:]]+2$")
-  BOOT_PART=$(lsblk -ln -o NAME /dev/"$DEVICE" | grep -E "${DEVICE}[^[:space:]]+3$")
-
+  ROOT_PART=$(lsblk -ln -o NAME /dev/"$DEVICE" | grep -E "${DEVICE}(p)?1$")
+  SWAP_PART=$(lsblk -ln -o NAME /dev/"$DEVICE" | grep -E "${DEVICE}(p)?2$")
+  BOOT_PART=$(lsblk -ln -o NAME /dev/"$DEVICE" | grep -E "${DEVICE}(p)?3$")
+  
+  if [ -z "$ROOT_PART" ] || [ -z "$SWAP_PART" ] || [ -z "$BOOT_PART" ]; then 
+    echo "Error: no Partitions found. Aborting!"
+    exit
+  fi
   echo "$ROOT_PART $SWAP_PART $BOOT_PART"
 
   #Formating
