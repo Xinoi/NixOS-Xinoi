66cb7d9625789bb3455c8b67152c55b81ed581da
Xinoi <zaunrithj@gmail.com>
2025-03-12 00:43:30 +0100

    virt manager

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,3 +1,6 @@
 { 
-  virtualisation.vmware.host.enable=true;
+  programs.virt-manager.enable = true; 
+  users.groups.libvirtd.members = ["xinoi"];
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
 }
