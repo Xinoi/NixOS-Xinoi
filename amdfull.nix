4b32099853ac4ae182bee0c93e59b6fff41d74d6
Xinoi <zaunrithj@gmail.com>
2025-03-11 21:23:24 +0100

    shell module

--- amdfull.nix
+++ amdfull.nix
@@ -64,29 +64,26 @@
   users.defaultUserShell = pkgs.zsh;
 
   programs.zsh = {
-  enable = true;
-  enableCompletion = true;
-  autosuggestions.enable = true;
-  syntaxHighlighting.enable = true;
-
-  shellAliases = {
-    ll = "ls -l";
-    oo = "cd /home/xinoi/nxcwy-movwy/Obsidian/Life";
-    h = "cd /home/xinoi";
-    flake-update = "(cd ~/NixOS-Xinoi; sudo nix flake update && sudo nixos-rebuild switch --flake .#amdfull)";
-    flake-config = "nvim ~/NixOS-Xinoi/flake.nix";
-    config = "nvim ~/NixOS-Xinoi/configuration.nix";
-    e = "exit";
-    pwo = "poweroff";
-    lg = "lazygit";
-    };
-
-  ohMyZsh = {
     enable = true;
-    plugins = [ "git" "cp" ];
-    theme = "darkblood";
-  };
-
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      oo = "cd /home/xinoi/nxcwy-movwy/Obsidian/Life";
+      h = "cd /home/xinoi";
+      flake-update = "(cd ~/NixOS-Xinoi; sudo nix flake update && sudo nixos-rebuild switch --flake .#amdfull)";
+      flake-config = "nvim ~/NixOS-Xinoi/flake.nix";
+      config = "nvim ~/NixOS-Xinoi/configuration.nix";
+      e = "exit";
+      pwo = "poweroff";
+      lg = "lazygit";
+      };
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" "cp" ];
+      theme = "darkblood";
+    };
   };
 
   #my services
