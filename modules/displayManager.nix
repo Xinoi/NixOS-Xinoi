7f1b23986a17328e5156acd42f12e4e3c1b277ae
Xinoi <zaunrithj@gmail.com>
2025-03-15 19:52:43 +0100

    astronaut

--- modules/displayManager.nix
+++ modules/displayManager.nix
@@ -1,20 +1,27 @@
 {pkgs, ...}:
-
-let 
-  astronaut = pkgs.sddm-astronaut; 
-in
 {
-  services.displayManager = {
-    sddm = {
-      enable = true;
-      package = pkgs.kdePackages.sddm;
-      wayland.enable = true;
-      theme = astronaut;
-      extraPackages = with pkgs; [
-        sddm-astronaut
+  environment.systemPackages = [
+      (pkgs.callPackage ./sddm-astronaut-theme.nix {
+          theme = "black_hole";
+    themeConfig={
+        General = {
+        HeaderText ="Hi";
+              Background="/home/xinoi/nxcwy-movwy/dotfiles/pixel_sakura_static.png";
+              FontSize="10.0";
+            };	
+        };
+        })
       ];
-    };
-    defaultSession = "hyprland";
+
+  services.displayManager.sddm = {
+    enable = true;
+    package = pkgs.kdePackages.sddm;
+    wayland.enable = true;
+    theme = "sddm-astronaut-theme";
+    extraPackages = with pkgs; [
+        kdePackages.qtmultimedia
+        kdePackages.qtsvg
+        kdePackages.qtvirtualkeyboard
+    ];
   };
 }
-
