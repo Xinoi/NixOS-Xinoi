65510d45145b84071b1dad8912a54b027b60b9f0
Johannes Zaunrith <zaunrithj@gmail.com>
2025-11-16 17:18:01 +0100

    sha update seafile

--- modules/seafile.nix
+++ modules/seafile.nix
@@ -2,7 +2,7 @@
 let 
   pkgs_2505 = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/nixos-25.05.tar.gz";
-    sha256 = "03zkw4rk7v2p2z43lfbig688i54a2822j45sfqdklr9rb0nqx3dx";
+    sha256 = "0bz1qwd1fw9v4hmxi6h2qfgvxpv4kwdiz7xd9p7j1msr0b8d54h3";
   }) {
     system = pkgs.system;
     config = config.nixpkgs.config;
