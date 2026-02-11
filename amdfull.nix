6a121ebf85edaa51c33805c1f0e873551252a469
Johannes <zaunrithj@gmail.com>
2026-02-11 02:55:40 +0100

    added disk coinfig to amdfull

--- amdfull.nix
+++ amdfull.nix
@@ -4,6 +4,8 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
+      ./disk-config.nix
       ./modules/displayManager.nix
       ./modules/hyprland.nix
       ./modules/drivers.nix
