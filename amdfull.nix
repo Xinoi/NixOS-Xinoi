b932c9d27fbfe825cfb16b95c4e05cf571c5f013
Johannes <zaunrithj@gmail.com>
2026-04-12 13:21:28 +0200

    cachix for walker

--- amdfull.nix
+++ amdfull.nix
@@ -98,13 +98,13 @@
 
   nix.settings = {
     substituters = [
-      "https://hyprland.cachix.org"
-      "https://nix-gaming.cachix.org"
+      "https://walker.cachix.org"
+      "https://walker-git.cachix.org"
       ];
 
     trusted-public-keys = [
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+      "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
     ];
   };
 
