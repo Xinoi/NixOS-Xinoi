df756f9075f5232387d6d1308cdc70651c5a583d
Johannes <zaunrithj@gmail.com>
2026-04-23 17:41:23 +0200

    soulbeet service

--- modules/services/music.nix
+++ modules/services/music.nix
@@ -11,14 +11,32 @@
     };
   };
 
-  services.slskd = {
-    enable = true;
-    user = "xinoi";
-    environmentFile = config.sops.secrets."slskd-env".path;
-    settings = {
-      directories.downloads = "/mnt/data/slskd/downloads";
-      directories.incomplete = "/mnt/data/slskd/incomplete";
+  systemd.services.soulbeet = {
+    description = "SoulBeet - Manage/Download music with beet and slskd";
+    after = [
+      "network-online.target"
+      "podman.service"
+      "navidrome.service"
+    ];
+    requires = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    path = [ pkgs.podman pkgs.docker-compose ];
+
+    serviceConfig = {
+      Type = "simple";
+      User = "xinoi";
+      WorkingDirectory = "/etc/soulbeet";
+      ExecStart = "${pkgs.podman}/bin/podman compose -f soulbeet-server.yml up";
+      ExecStop = "${pkgs.podman}/bin/podman compose -f soulbeet-server.yml down";
+      Restart = "always";
     };
+  };
+
+  environment.etc."soulbeet/soulbeet-server.yml" = {
+    source = ../../container/soulbeet/soulbeet-server.yml;
+    mode = "0440";
+    user = "xinoi";
   };
 
 }
